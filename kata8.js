6eed6be5b9f619c1c69302840f67a41ac50d9c10
VladTca <votsats@gmail.com>
2023-12-19 17:25:33 +0100

    first attempt in codewar. ex1

--- kata8.js
+++ kata8.js
@@ -2,3 +2,21 @@
 function greet(name){
     return "Hello, " + name +  " how are you doing today?"
 }
+
+//--------------------------------------
+// ex2
+const isAlpha = function(ch){
+    return typeof ch === "string" && ch.length === 1
+        && (ch >= "a" && ch <= "z" || ch >= "A" && ch <= "Z");
+}
+function sameCase(a, b){
+    if (isAlpha(a) !== true ||  isAlpha(b)  !== true ){
+
+        return -1;}
+    else if ( a.toUpperCase() !== a && b.toUpperCase() !== b ) {
+        //console.log('wow')
+        return 1}
+    if (a.toUpperCase() === a && b.toUpperCase() === b) {
+        return 1}
+    else  {return 0;}
+}
