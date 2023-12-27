3bc1a06d0e3b30bdb9b1538c7beeeb6154cf7505
VladTca <votsats@gmail.com>
2023-12-27 20:46:39 +0100

    codewars go!

--- kata8.js
+++ kata8.js
@@ -119,9 +119,9 @@ function remove (string) {
 }
 console.log(remove('HI! Hi!!!wsxedc!!!'))
 
-// const remove = s => s.replace(/!$/, '');   $ -posledniq znak esli posle i pervii esli do
+// const remove = s => s.replace(/!$/, '');  ---------- $ -posledniq znak esli posle i pervii esli do
 //ili
-//   return s.endsWith('!') ? s.slice(0, -1) : s; endswith true/false dlya konca stroki
+//   return s.endsWith('!') ? s.slice(0, -1) : s; -------------endswith true/false dlya konca stroki
 
 //-----------------------------------------------------------
 
@@ -157,3 +157,95 @@ console.log(evil(123))
 //function evil(n) {
 //     return n.toString(2).match(/1/g).length % 2 ? 'It\'s Odious!' : 'It\'s Evil!';
 // }
+
+//---------------------------------------------
+function defineSuit(card) {
+    let arr = Array.from(card);
+    //let s=card.endsWith('♣');
+    switch (arr[arr.length-1]) {
+        case '♣' : return 'clubs';break;
+        case '♦' : return 'diamonds';break;
+        case '♥' : return 'hearts';break;
+        case '♠' : return 'spades';break;
+    }
+}
+console.log(defineSuit('3♣'))
+
+/*function defineSuit(card) {
+  switch (card.slice(-1)){
+    case '♣': return 'clubs';
+    case '♦': return 'diamonds';
+    case '♥': return 'hearts';
+    case '♠': return 'spades';
+  }
+}*/
+/*function defineSuit(card) {
+  return {
+    '♣' : 'clubs',
+    '♦' : 'diamonds',
+    '♥' : 'hearts',
+    '♠' : 'spades'
+  }[card.substr(-1)]
+}*/
+/*function defineSuit(card) {
+  const s = {
+    "♣": "clubs",
+    "♠": "spades",
+    "♦": "diamonds",
+    "♥": "hearts"
+  }
+  return s[card.charAt(card.length - 1)]
+}*/
+
+//---------------------------
+function  calculateAge(x,y) {
+    if(x===y) return "You were born this very year!"
+    else {
+        if (x<y && (y-x)===1) return ("You are "+(y-x)+" year old.");
+        else if (x<y) return ("You are "+(y-x)+" years old.");
+        else if(x>y && (x-y)===1) return ("You will be born in "+(x-y)+" year.");
+        else return ("You will be born in "+(x-y)+" years.");
+    }
+}
+console.log(calculateAge(2015,2012))
+
+/*function  calculateAge(m, n) {
+	if(m == n) return 'You were born this very year!';
+  var year = Math.abs(m-n) == 1 ? 'year' : 'years';
+  if(m < n) return "You are "+(n-m)+' '+year+' old.';
+  if(m > n) return "You will be born in "+(-n+m)+' '+year+'.';
+}*/
+
+//--------------------------------
+
+function remove (string) {
+    for(let i=0;i<string.length;i++){
+        if (string.endsWith('!')===true) string=string.slice(0,-1);
+
+    }
+    return string
+}
+
+//  return s.replace(/!+$/, '');
+
+const firstName = {A: 'Alpha', B: 'Beta', C: 'Cache'};
+const surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst'};
+
+function aliasGen(a,b){
+    let x= a.slice(0,1).toUpperCase(); //.toUpperCase();
+    let y=b.slice(0,1).toUpperCase(); //.toUpperCase();
+    const result = firstName[x] +' '+ surname[y];
+    const regex =/[A-Z]/
+    console.log(regex.test(x));
+    console.log(regex.test(y));
+    //let z=x.match(/[A-Z]/)
+    //console.log(z);
+    //let arr=new Array[A-Z];
+    //if (arr.find(x)===true && arr.find(y)===true) return result;
+    if(regex.test(x)===true && regex.test(y)===true)
+    {return result;
+        console.log(result);}
+    else return "Your name must start with a letter from A - Z."
+
+}
+console.log(aliasGen('Cbram','Bendera'))
--- test.js
+++ test.js
@@ -1,3 +1,21 @@
-function logs(x , a, b){
-    return getBaseLog(x,a)+getBaseLog(x,b)
+const firstName = {A: 'Alpha', B: 'Beta', C: 'Cache'};
+const surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst'};
+
+function aliasGen(a,b){
+    let x= a.slice(0,1).toUpperCase(); //.toUpperCase();
+    let y=b.slice(0,1).toUpperCase(); //.toUpperCase();
+    const result = firstName[x] +' '+ surname[y];
+    const regex =/[A-Z]/
+    console.log(regex.test(x));
+    console.log(regex.test(y));
+   //let z=x.match(/[A-Z]/)
+    //console.log(z);
+        //let arr=new Array[A-Z];
+    //if (arr.find(x)===true && arr.find(y)===true) return result;
+    if(regex.test(x)===true && regex.test(y)===true)
+    {return result;
+    console.log(result);}
+   else return "Your name must start with a letter from A - Z."
+
 }
+console.log(aliasGen('Cbram','Bendera'))
