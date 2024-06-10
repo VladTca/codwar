7024886e17908c457416aa499e128d03a0788211
VladTca <votsats@gmail.com>
2024-06-10 21:07:46 +0200

    🎨 Improve structure / format of the code.

---
+++ kata8/Sum_without_highest_and_lowest _umber.js
@@ -0,0 +1,22 @@
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+//
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+//
+//     Mind the input validation.
+//
+//     Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+
+let arr = [];
+
+function sumArray(array) {
+  return Array.isArray(array) && array.length > 1
+    ? array
+        .sort((a, b) => a - b)
+        .slice(1, -1)
+        .reduce((acc, i) => acc + i, 0)
+    : 0;
+}
+
+console.log(sumArray(arr));
--- test.js
+++ test.js
@@ -1,38 +1,12 @@
-let a = "abccccdd";
+heights = [1, 1, 4, 2, 1, 3];
 
-var longestPalindrome = function (s) {
-  count = 0;
-  res = [];
-  for (let i = 0; i < s.length; i++) {
-    if (s.indexOf(s[i]) !== s.lastIndexOf(s[i])) count++;
+var heightChecker = function (heights) {
+  let expected = [...heights].sort((a, b) => a - b);
+  let count = 0;
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] !== expected[i]) count++;
   }
-  if (s.length > count) count++;
   return count;
 };
 
-console.log(longestPalindrome(a));
-
-//
-// let a = "abccccdd";
-//
-// var longestPalindrome = function (s) {
-//   let obj = {};
-//   let count = 0;
-//   for (let i = 0; i < s.length; i++) {
-//     if (obj[s[i]] === undefined) {
-//       obj[s[i]] = 1;
-//     } else {
-//       obj[s[i]]++;
-//     }
-//   }
-//   for (let i in obj) {
-//     if (obj[i] % 2 === 0) {
-//       count += obj[i];
-//     } else {
-//       count += Math.floor(obj[i] / 2) * 2;
-//     }
-//   }
-//   return count;
-// };
-//
-// console.log(longestPalindrome(a));
+console.log(heightChecker(heights));
