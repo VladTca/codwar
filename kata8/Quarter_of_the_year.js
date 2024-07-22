48aa30a4b98aaba76eef29b90cddcc644650cd86
VladTca <votsats@gmail.com>
2024-07-22 23:26:23 +0200

    🎨 Improve structure / format of the code.

---
+++ kata8/Quarter_of_the_year.js
@@ -0,0 +1,15 @@
+const quarterOf = (month) => {
+    // Your code here
+    switch (true) {
+        case (month >= 1 && month <= 3):
+            return 1;
+        case (month >= 4 && month <= 6):
+            return 2;
+        case (month >= 7 && month <= 9):
+            return 3;
+        case (month >= 10 && month <= 12):
+            return 4;
+        default:
+            throw new Error("Invalid month value");
+    }
+}
