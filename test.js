94c3a05cb317d0e4decc0a323848a83dd8ba545c
VladTca <votsats@gmail.com>
2024-08-15 11:03:05 +0200

    ✨ 🎨 Improve structure/format of the code.

--- test.js
+++ test.js
@@ -1,35 +1,15 @@
-// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
-//
-//     We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
-//
-//     You must solve this problem without using the library's sort function.
-//
-//
-//
-// Example 1:
-//
-// Input: nums = [2,0,2,1,1,0]
-// Output: [0,0,1,1,2,2]
-// Example 2:
-//
-// Input: nums = [2,0,1]
-// Output: [0,1,2]
-//
+function* myGenerator() {
+  let a = yield "Первое значение";
+  console.log("a:", a);
 
-var sortColors = function (nums) {
-  let red = nums.filter((a) => a === 0);
-  let white = nums.filter((a) => a === 1);
-  let blue = nums.filter((a) => a === 2);
-  return red.concat(white.concat(blue));
-};
+  let b = yield "Второе значение";
+  console.log("b:", b);
 
-// numsq = [2, 0, 1];
+  return "Конец";
+}
 
-// var sortColors = function (nums) {
-//   let red = nums.filter((a) => a === 0);
-//   let white = nums.filter((a) => a === 1);
-//   let blue = nums.filter((a) => a === 2);
-//   return red.concat(white.concat(blue));
-// };
-//
-// console.log(sortColors(numsq));
+const gen = myGenerator();
+
+console.log(gen.next()); // { value: "Первое значение", done: false }
+console.log(gen.next(10)); // "a: 10" => { value: "Второе значение", done: false }
+console.log(gen.next(20)); // "b: 20" => { value: "Конец", done: true }
